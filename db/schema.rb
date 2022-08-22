34504c7bba64eaa42dc7bd2fc20ad38cf0f2a7e7
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-22 21:57:37 +0200

    models offer reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_194440) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_195315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.float "price_per_night"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "offer_id", null: false
+    t.bigint "user_id", null: false
+    t.date "date_checkin"
+    t.date "date_checkout"
+    t.integer "guests"
+    t.boolean "confirmed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_reservations_on_offer_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_194440) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offers", "users"
+  add_foreign_key "reservations", "offers"
+  add_foreign_key "reservations", "users"
 end
