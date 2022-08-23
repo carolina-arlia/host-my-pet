4db0b7fd57a96f0ff0b88667cfab7919b78ec4e7
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-23 15:00:19 -0300

    delete

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -33,6 +33,10 @@ class OffersController < ApplicationController
   end
 
   def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to offers_path, status: :see_other
   end
 
   private
