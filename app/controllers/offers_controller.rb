1f0aad19db2fea1ae3f90ebc307adcaf328f834c
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-23 14:32:41 -0300

    offer new, create

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,4 +8,32 @@ class OffersController < ApplicationController
     @offer = Offer.find(params[:id])
     @reservation = Reservation.new
   end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user_id = current_user.id
+    @offer.save
+
+    redirect_to offers_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :location, :price_per_night)
+  end
+
 end
