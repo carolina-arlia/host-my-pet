1cccf3057eebd908bb1409a6a6be49b448bad16c
Clari Ursini <clari_ursini@hotmail.com>
2022-08-24 18:44:49 -0300

    Updates in nav, fixing errors

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,7 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations
   has_many_attached :photos
+  validates :photos, presence: true
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   end
   resources :reservations, only: [:destroy, :update, :edit, :index, :show]
   get 'my_reservations', to: 'reservations#my_reservations', as: 'my_reservations'
+
   get 'my_offers', to: 'offers#my_offers', as: 'my_offers'
+  get 'my_offers/:id', to: 'offers#my_offers_show', as: 'my_offers_show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
