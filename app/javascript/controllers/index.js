f2e8045ce995fffefd89a97ae351ac55327886ea
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-26 02:32:46 +0200

    typed.js running

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,3 +15,6 @@ application.register("location-autocomplete", LocationAutocompleteController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
